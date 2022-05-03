1338001c44b23745712a3df7206ea4be5144ad78
Mahindra patil <mahendra.patil@aureatelabs.com>
2022-05-03 10:54:19 +0530

    added new commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
         <h1>{{ msg }}</h1> 
         <p>Hey new here</p> 
         <h1>{{ msg }}</h1>
-        <h3>Installed CLI Plugins.</h3>
+        <h3>Installed CLI Plugins. new commit</h3>
         <p>
             For a guide and recipes on how to configure / customize this project,<br> check out the
             <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
